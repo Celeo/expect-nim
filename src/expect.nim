f800799de51224b57c48a8e1013ba8dfded01f70
Celeo <celeodor@gmail.com>
2019-12-23 18:14:16 -0800

    docstrings

--- src/expect.nim
+++ src/expect.nim
@@ -19,43 +19,73 @@ proc message(t: ExpectType, a: auto, b: auto): string =
   &"\n===== Expect Error =====\nLeft: {a}, right: {b}\nChecked for: {t}\n========================\n"
 
 proc expectTrue*(a: bool) =
+  ## Assert that the passed value is true.
+  ##
+  ## An `ExpectError` is raised if this is false.
   let b = true
   if a != b:
     raise newException(ExpectError, message(etTrue, a, true))
 
 proc expectFalse*(a: bool) =
+  ## Assert that the passed value is false.
+  ##
+  ## An `ExpectError` is raised if this is false.
   let b = false
   if a != b:
     raise newException(ExpectError, message(etFalse, a, false))
 
 proc expectEqual*(a, b: auto) =
+  ## Assert that both values are equal.
+  ##
+  ## An `ExpectError` is raised if this is false.
   if a != b:
     raise newException(ExpectError, message(etEqual, a, b))
 
 proc expectNotEqual*(a, b: auto) =
+  ## Assert that the values are not equal.
+  ##
+  ## An `ExpectError` is raised if this is false.
   if a == b:
     raise newException(ExpectError, message(etNotEqual, a, b))
 
 proc expectStringEqual*(a, b: auto) =
+  ## Assert that the string representations of the values are equal.
+  ##
+  ## An `ExpectError` is raised if this is false.
   if $a != $b:
     raise newException(ExpectError, message(etStringEqual, $a, $b))
 
 proc expectStringNotEqual*(a, b: auto) =
+  ## Assert that the string representations of the values are not equal.
+  ##
+  ## An `ExpectError` is raised if this is false.
   if $a == $b:
     raise newException(ExpectError, message(etStringNotEqual, $a, $b))
 
 proc expectLessThan*(a, b: SomeNumber) =
+  ## Assert that the second value is less than the first.
+  ##
+  ## An `ExpectError` is raised if this is false.
   if a >= b:
     raise newException(ExpectError, message(etLessThan, a, b))
 
 proc expectLessThanEqual*(a, b: SomeNumber) =
+  ## Assert that the second value is less than or equal to the first.
+  ##
+  ## An `ExpectError` is raised if this is false.
   if a > b:
     raise newException(ExpectError, message(etLessThanEqual, a, b))
 
 proc expectGreaterThan*(a, b: SomeNumber) =
+  ## Assert that the second value is greater than the first.
+  ##
+  ## An `ExpectError` is raised if this is false.
   if a <= b:
     raise newException(ExpectError, message(etGreaterThan, a, b))
 
 proc expectGreaterThanEqual*(a, b: SomeNumber) =
+  ## Assert that the second value is greater than or equal to the first.
+  ##
+  ## An `ExpectError` is raised if this is false.
   if a < b:
     raise newException(ExpectError, message(etGreaterThanEqual, a, b))
