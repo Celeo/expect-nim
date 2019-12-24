911dcc748e7322837c1ad2af840f48c869dcfc34
Celeo <celeodor@gmail.com>
2019-12-23 18:47:00 -0800

    add raises pragma

--- src/expect.nim
+++ src/expect.nim
@@ -16,9 +16,10 @@ type
     etGreaterThanEqual = "greater than or equal"
 
 proc message(t: ExpectType, a: auto, b: auto): string =
-  &"\n===== Expect Error =====\nLeft: {a}, right: {b}\nChecked for: {t}\n========================\n"
+  "\n===== Expect Error =====\nLeft: " & $a & ", right: " & $b &
+      "\nChecked for: " & $t & "\n========================\n"
 
-proc expectTrue*(a: bool) =
+proc expectTrue*(a: bool) {.raises: [ExpectError].} =
   ## Assert that the passed value is true.
   ##
   ## An `ExpectError` is raised if this is false.
@@ -26,7 +27,7 @@ proc expectTrue*(a: bool) =
   if a != b:
     raise newException(ExpectError, message(etTrue, a, true))
 
-proc expectFalse*(a: bool) =
+proc expectFalse*(a: bool) {.raises: [ExpectError].} =
   ## Assert that the passed value is false.
   ##
   ## An `ExpectError` is raised if this is false.
@@ -34,56 +35,56 @@ proc expectFalse*(a: bool) =
   if a != b:
     raise newException(ExpectError, message(etFalse, a, false))
 
-proc expectEqual*(a, b: auto) =
+proc expectEqual*(a, b: auto) {.raises: [ExpectError].} =
   ## Assert that both values are equal.
   ##
   ## An `ExpectError` is raised if this is false.
   if a != b:
     raise newException(ExpectError, message(etEqual, a, b))
 
-proc expectNotEqual*(a, b: auto) =
+proc expectNotEqual*(a, b: auto) {.raises: [ExpectError].} =
   ## Assert that the values are not equal.
   ##
   ## An `ExpectError` is raised if this is false.
   if a == b:
     raise newException(ExpectError, message(etNotEqual, a, b))
 
-proc expectStringEqual*(a, b: auto) =
+proc expectStringEqual*(a, b: auto) {.raises: [ExpectError].} =
   ## Assert that the string representations of the values are equal.
   ##
   ## An `ExpectError` is raised if this is false.
   if $a != $b:
     raise newException(ExpectError, message(etStringEqual, $a, $b))
 
-proc expectStringNotEqual*(a, b: auto) =
+proc expectStringNotEqual*(a, b: auto) {.raises: [ExpectError].} =
   ## Assert that the string representations of the values are not equal.
   ##
   ## An `ExpectError` is raised if this is false.
   if $a == $b:
     raise newException(ExpectError, message(etStringNotEqual, $a, $b))
 
-proc expectLessThan*(a, b: SomeNumber) =
+proc expectLessThan*(a, b: SomeNumber) {.raises: [ExpectError].} =
   ## Assert that the second value is less than the first.
   ##
   ## An `ExpectError` is raised if this is false.
   if a >= b:
     raise newException(ExpectError, message(etLessThan, a, b))
 
-proc expectLessThanEqual*(a, b: SomeNumber) =
+proc expectLessThanEqual*(a, b: SomeNumber) {.raises: [ExpectError].} =
   ## Assert that the second value is less than or equal to the first.
   ##
   ## An `ExpectError` is raised if this is false.
   if a > b:
     raise newException(ExpectError, message(etLessThanEqual, a, b))
 
-proc expectGreaterThan*(a, b: SomeNumber) =
+proc expectGreaterThan*(a, b: SomeNumber) {.raises: [ExpectError].} =
   ## Assert that the second value is greater than the first.
   ##
   ## An `ExpectError` is raised if this is false.
   if a <= b:
     raise newException(ExpectError, message(etGreaterThan, a, b))
 
-proc expectGreaterThanEqual*(a, b: SomeNumber) =
+proc expectGreaterThanEqual*(a, b: SomeNumber) {.raises: [ExpectError].} =
   ## Assert that the second value is greater than or equal to the first.
   ##
   ## An `ExpectError` is raised if this is false.
